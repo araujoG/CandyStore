a9a61afdc5b13b3a80b8dc2c881594f59049bd2a
araujoG <araujo_gabriel@id.uff.br>
2020-10-05 02:47:01 -0300

    4paginas finalizadas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,24 @@
-$(function(){
+$(function () {
     $('[data-toggle = "tooltip"]').tooltip();
     /*procura estruturas com o atrivuto data-toogle = tooltip e executa o método .tooltip() em todas elas */
     $('[data-toggle = "popover"]').popover();
+    var quantitiy = 0;
+    $('.diminuiQuantidade').click(function (e) {
+        campo = $(this).attr('data-field');
+        e.preventDefault();
+        var quantity = parseInt($('#' + campo + '').val());
+        $('#' + campo + '').val(quantity - 1);
+    });
+
+    $('.aumentaQuantidade').click(function (e) {
+        campo = $(this).attr('data-field');
+        e.preventDefault();
+        var quantity = parseInt($('#' + campo + '').val());
+        $('#' + campo + '').val(quantity + 1);
+
+    });
+
 })
 $('.carousel').carousel({
     interval: 4000
-  })
+})
