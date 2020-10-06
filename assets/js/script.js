97abb7178070b711af61efad3b9ba5451c9793e7
araujoG <araujo_gabriel@id.uff.br>
2020-10-05 23:04:35 -0300

    trabalho 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,22 @@
 $(function () {
+    $('.alert').alert()
     var quantitiy = 0;
     $('.diminuiQuantidade').click(function (e) {
         campo = $(this).attr('data-field');
         e.preventDefault();
         var quantity = parseInt($('#' + campo + '').val());
-        $('#' + campo + '').val(quantity - 1);
+        if (quantity > 1) {
+            $('#' + campo + '').val(quantity - 1);
+        }
     });
 
     $('.aumentaQuantidade').click(function (e) {
         campo = $(this).attr('data-field');
         e.preventDefault();
         var quantity = parseInt($('#' + campo + '').val());
-        $('#' + campo + '').val(quantity + 1);
-
+        if (quantity < 999) {
+            $('#' + campo + '').val(quantity + 1);
+        }
     });
 
 })
