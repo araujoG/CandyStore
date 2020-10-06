27e53de7780440572b6408fb9de9770439f290be
araujoG <araujo_gabriel@id.uff.br>
2020-10-05 22:41:39 -0300

    funções inutilizadas do js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,4 @@
 $(function () {
-    $('[data-toggle = "tooltip"]').tooltip();
-    /*procura estruturas com o atrivuto data-toogle = tooltip e executa o método .tooltip() em todas elas */
-    $('[data-toggle = "popover"]').popover();
     var quantitiy = 0;
     $('.diminuiQuantidade').click(function (e) {
         campo = $(this).attr('data-field');
