28a2510c9f83689e41351564ab68f6236289916f
araujoG <araujo_gabriel@id.uff.br>
2020-11-11 04:26:11 -0300

    trabalho 3 recuperado

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,17 +4,63 @@ $(document).ready(function () {
     $('#inputTelefone').mask('(00) 00000-0000');
     $('#inputCpf').mask('000.000.000-00');
     $('#inputCep').mask('00.000-000');
-    $('#inputNumero').mask('0#')
+    $('#inputCepFrete').mask('00.000-000');
+    $('#inputNumero').mask('0#');
+    $("a[href='#']").attr("href", "javascript:void(0);");
+    
 });
 
 $(function () {
+    $("#starRateAvg").rateYo({
+        readOnly: true,
+        rating: 3.5,
+        maxValue: 5,
+        numStars: 5,
+        starWidth: "25px"
+    });
+    var ratingAvg = $("#starRateAvg").rateYo("option", "rating");
+    document.getElementById('starRateNumberAvg').innerHTML = "(" + ratingAvg + ")";
+
+    $("#starRate1").rateYo({
+        readOnly: true,
+        rating: 3,
+        maxValue: 5,
+        numStars: 5,
+        starWidth: "15px"
+    });
+    var rating1 = $("#starRate1").rateYo("option", "rating");
+    document.getElementById('starRate1Number').innerHTML = "(" + rating1 + ")";
+
+    $("#starRate2").rateYo({
+        readOnly: true,
+        rating: 4,
+        maxValue: 5,
+        numStars: 5,
+        starWidth: "15px"
+    });
+    var rating2 = $("#starRate2").rateYo("option", "rating");
+    document.getElementById('starRate2Number').innerHTML = "(" + rating2 + ")";
+
+    $("#starRate3").rateYo({
+        readOnly: true,
+        rating: 3.5,
+        maxValue: 5,
+        numStars: 5,
+        starWidth: "15px"
+    });
+    var rating3 = $("#starRate3").rateYo("option", "rating");
+    document.getElementById('starRate3Number').innerHTML = "(" + rating3 + ")";
+})
+
+$(function () {
+
     $('.carousel').carousel({
         interval: 4000
-    })
+    });
 
     $('.toast').toast('show');
 
-    $('.alert').alert()
+    $('.alert').alert();
 
     var quantitiy = 0;
     $('.diminuiQuantidade').click(function (e) {
@@ -36,27 +82,97 @@ $(function () {
     });
 
     $("#finalizarCadastro").click(function () {
-        let nomeValido = validaIsEmpty($('#inputNome'))
-        let sobrenomeValido = validaIsEmpty($('#inputSobrenome'))
-        let emailValido = validaEmail($('#inputEmail'))
-        let senhaValida = validaSenha($('#inputSenha'))
-        let confirmaSenhaValido = validaConfirmaSenha($('#inputSenha'), $('#inputConfirmaSenha'))
-        let cpfValido = validaCpf($('#inputCpf'))
-        let telfoneValido = validaTelefone($('#inputTelefone'))
-        let nascimentoValido = validaIsEmpty($('#inputNascimento'))
-        let sexoValido = validaSexo($('#inputSexo'))
-        let cepValido = validaIsEmpty($('#inputCep'))
-        let estadoValido = validaIsEmpty($('#inputEstado'))
-        let cidadeValido = validaIsEmpty($('#inputCidade'))
-        let bairroValido = validaIsEmpty($('#inputBairro'))
-        let enderecoValido = validaIsEmpty($('#inputEndereco'))
-        let numeroValido = validaIsEmpty($('#inputNumero'))
+        let nomeValido = validaIsEmpty($('#inputNome'));
+        let sobrenomeValido = validaIsEmpty($('#inputSobrenome'));
+        let emailValido = validaEmail($('#inputEmail'));
+        let senhaValida = validaSenha($('#inputSenha'));
+        let confirmaSenhaValido = validaConfirmaSenha($('#inputSenha'), $('#inputConfirmaSenha'));
+        let cpfValido = validaCpf($('#inputCpf'));
+        let telfoneValido = validaTelefone($('#inputTelefone'));
+        let nascimentoValido = validaIsEmpty($('#inputNascimento'));
+        let sexoValido = validaSexo($('#inputSexo'));
+        let cepValido = validaIsEmpty($('#inputCep'));
+        let estadoValido = validaIsEmpty($('#inputEstado'));
+        let cidadeValido = validaIsEmpty($('#inputCidade'));
+        let bairroValido = validaIsEmpty($('#inputBairro'));
+        let enderecoValido = validaIsEmpty($('#inputEndereco'));
+        let numeroValido = validaIsEmpty($('#inputNumero'));
+    })
 
+
+    $('#avRate1').ready(function () {
+        let like = $("#avRate1"+ " #like").data("like");
+        let dislike = $("#avRate1" + " #dislike").data("dislike");
+        $("#avRate1" + " #like").text("(" + like + ")");
+        $("#avRate1" + " #dislike").text("(" + dislike + ")");
+    });
+
+    $('#avRate2').ready(function () {
+        let like = $("#avRate2"+ " #like").data("like");
+        let dislike = $("#avRate2" + " #dislike").data("dislike");
+        $("#avRate2" + " #like").text("(" + like + ")");
+        $("#avRate2" + " #dislike").text("(" + dislike + ")");
+    });
+
+    $('#avRate3').ready(function () {
+        let like = $("#avRate3"+ " #like").data("like");
+        let dislike = $("#avRate3" + " #dislike").data("dislike");
+        $("#avRate3" + " #like").text("(" + like + ")");
+        $("#avRate3" + " #dislike").text("(" + dislike + ")");
+    });
+
+    $(".likeBtn").click(function () {
+        field = $(this).attr('data-field');
+        like = $(field + " #like").data("like");
+        dislike = $(field + " #dislike").data("dislike");
+
+        if ($(field + " #likeBtn").data('clicked') == 0) {
+            $(field + " #likeBtn i").toggleClass('far fas');
+            $(field + " #likeBtn").data('clicked', 1);
+            like = like + 1;
+            if ($(field + " #dislikeBtn").data('clicked') == '1') {
+                $(field + " #dislikeBtn i").toggleClass('fas far');
+                dislike = dislike - 1;
+                $(field + " #dislikeBtn").data('clicked', 0);
+            }
+        } else {
+            $(field + " #likeBtn i").toggleClass('fas far');
+            like = like - 1;
+            $(field + " #likeBtn").data('clicked', 0);
+        }
+        $(field + " #like").data('like', like);
+        $(field + " #dislike").data('dislike', dislike);
+        $(field + " #like").text("(" + like + ")");
+        $(field + " #dislike").text("(" + dislike + ")");
+    })
+
+    $(".dislikeBtn").click(function () {
         
-
+        field = $(this).attr('data-field');
+        like = $(field + " #like").data("like");
+        dislike = $(field + " #dislike").data("dislike");
+        if ($(field + " #dislikeBtn").data('clicked') == 0) {
+            $(field + " #dislikeBtn i").toggleClass('far fas');
+            $(field + " #dislikeBtn").data('clicked', 1);
+            dislike = dislike + 1;
+            if ($(field + " #likeBtn").data('clicked') == '1') {
+                like = like - 1;
+                $(field + " #likeBtn").data('clicked', 0);
+                $(field + " #likeBtn i").toggleClass('fas far');
+            }
+        } else {
+            $(field + " #dislikeBtn i").toggleClass('fas far');
+            dislike = dislike - 1;
+            $(field + " #dislikeBtn").data('clicked', 0);
+        }
+        $(field + " #like").data('like', like);
+        $(field + " #dislike").data('dislike', dislike);
+        $(field + " #like").text("(" + like + ")");
+        $(field + " #dislike").text("(" + dislike + ")");
     })
 
 })
+
 
 
 function validaSenha(senha) {
@@ -74,7 +190,7 @@ function validaSenha(senha) {
 }
 
 function validaConfirmaSenha(senha, confirmaSenha) {
-    if (confirmaSenha.val() != senha.val() || confirmaSenha.val()=="") {
+    if (confirmaSenha.val() != senha.val() || confirmaSenha.val() == "") {
         confirmaSenha.addClass("is-invalid")
         confirmaSenha.removeClass("is-valid")
         return false
